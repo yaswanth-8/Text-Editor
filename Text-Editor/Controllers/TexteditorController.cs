c3010a221b862a6e9ebadbb70d0215765a91b7ca
yaswanth <ya8wanth@gmail.com>
2023-04-17 15:41:04 +0530

    Add project files.

---
+++ Text-Editor/Controllers/TexteditorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Text_Editor.Controllers
+{
+	public class TexteditorController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ Text-Editor/Models/TextEditor.cs
@@ -0,0 +1,9 @@
+﻿namespace Text_Editor.Models
+{
+	public class TextEditor
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Content { get; set; }
+	}
+}
