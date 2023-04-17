cb4a10e767bbfafddad739aa23f40d8947844f6e
yaswanth <ya8wanth@gmail.com>
2023-04-17 15:57:01 +0530

    Create text completed

--- Text-Editor/Controllers/TexteditorController.cs
+++ Text-Editor/Controllers/TexteditorController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using Text_Editor.Models;
 
 namespace Text_Editor.Controllers
 {
 	public class TexteditorController : Controller
 	{
-		public IActionResult Index()
+        IConfiguration _configuration;
+        public TexteditorController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            //_Connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public IActionResult Index()
 		{
 			return View();
 		}
-	}
+
+		[HttpPost]
+        public IActionResult Index(TextEditor obj)
+        {
+            SqlConnection _Connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+            _Connection.Open();
+            SqlCommand cmd = new SqlCommand("InsertData", _Connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@name", obj.Name);
+            cmd.Parameters.AddWithValue("@content", obj.Content);
+            cmd.ExecuteNonQuery();
+            _Connection.Close();
+            return View();
+        }
+    }
 }
